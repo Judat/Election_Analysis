5bf82d82ff36dba8b950e2b8e9d188db3d54e15e
Judat Ijaz <judatijaz@gmail.com>
2021-11-13 15:31:40 -0500

    added python file

---
+++ Python_Practice.py
@@ -0,0 +1,23 @@
+# print("Hello World !")
+# print(2+3)
+# print(2.8//5-3)
+# x = 3.8 +22*2-4
+# print(x)
+# print(8//5)
+# countries = ["Arapahoe" , "Denver", "Jefferson"]
+# countries[0]
+# print(countries[2])
+# countries[0]
+# type(2)
+# type("hello")
+# type(2)
+# type("")
+# print ("wah")
+# help("keywords")
+print((5 + 2) * 3)
+print((8 // 5) - 3)
+print (8 + (22 * (2 - 4)))
+print(16 - 3 / (2 + 7) - 1)
+print(3 ** (3 % 5))
+print(5 + (9 * 3 / 2 - 4) )
+print(5 + (9 * 3 / (2 - 4)))
