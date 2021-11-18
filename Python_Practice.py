154652aecede2a5a4803867f37f724d74eb75686
Judat Ijaz <judatijaz@gmail.com>
2021-11-17 23:21:40 -0500

    "Electyion Analysis Practice"

--- Python_Practice.py
+++ Python_Practice.py
@@ -43,5 +43,6 @@ voting_data.append({"county":"Arapahoe" , "registered_voters": 422829})
 voting_data.append ({"county": "Denver", "registered_voters": 463353})
 voting_data.append({"county": "Jefferson", "registered_voters": 432438})
 print(voting_data)
+print(counties_dict)
 
 
