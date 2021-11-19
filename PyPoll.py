25632d4cb3c76a2eb1915ee8afee4b58f9430741
Judat Ijaz <judatijaz@gmail.com>
2021-11-19 00:05:14 -0500

    Updated Election Audit PyPoll Challenge

--- PyPoll.py
+++ PyPoll.py
@@ -1,16 +1,4 @@
-# The Data we need to retreive
-# 1.Total number of votes cast
-# 2.A complete list of candidates who received votes
-# 3.Total number of votes each candidate received
-# 4.Percentage of votes each candidate won
-# 5.The winner of the election based on popular vote
-# import datetime
-# now = datetime.datetime.now()
-# print("The time right now is ", now)
 
-# import datetime as dt
-# now = dt.datetime.now()
-# print("the time right now is " , now)
 import csv
 import os
 # dir (csv)
